fb2c5da9ba0a52fa93bffd68ad1791f033b98dd0
Johan Lozano Dev <ankerjohan@gmail.com>
2026-01-27 19:12:58 +0100

    V 1.0.1

---
+++ src/js/app.js
@@ -0,0 +1,95 @@
+document.addEventListener ( "DOMContentLoaded", function(){
+    iniciarApp()
+});
+
+
+function iniciarApp() {
+    barraFija();
+    crearGaleria();
+    scrollNav();
+};
+function barraFija() {
+    const barra = document.querySelector('.header');
+    const inicio = document.querySelector('.header h1');
+    const sobreFestival = document.querySelector ('.sobre-festival');   
+    const body = document.querySelector('body');
+    
+    window.addEventListener( 'scroll', function() {
+        if  (sobreFestival.getBoundingClientRect().bottom < 0 ){    
+            barra.classList.add ('fijar-barra');  
+            body.classList.add ('body-scroll',);
+
+        }
+        else {
+            barra.classList.remove ('fijar-barra');   
+            body.classList.remove ('body-scroll');       
+        }
+    })
+
+} 
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll ('.navegacion-principal a');
+    
+    enlaces.forEach( enlace => {
+       enlace.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const seccionScroll = e.target.attributes.href.value;
+        const seccion = document.querySelector (seccionScroll);
+        seccion.scrollIntoView({ behavior:"smooth"}); 
+       });
+    }); 
+}
+
+function crearGaleria() {
+    const galeria = document.querySelector (".galeria-imagenes");
+ 
+    for ( let i = 1; i <= 12 ; i++ ){
+        const imagen = document.createElement ("picture");
+        imagen.innerHTML = `
+            <source srcset="build/img/thumb/${i}.avif" type="imagen/avif">
+            <source srcset="build/img/thumb/${i}.webp" type="imagen/webp">
+            <img loading="lazy" width="300" height="200" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
+        `;   
+
+        imagen.onclick = function() {
+            zoomImagen(i)
+        }
+        galeria.appendChild(imagen)
+    }
+}
+
+function zoomImagen(id) {
+    const imagen = document.createElement ("picture");
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="imagen/avif">
+        <source srcset="build/img/grande/${id}.webp" type="imagen/webp">
+        <img loading="lazy" width="300" height="200" src="build/img/grande/${id}.jpg" alt="imagen galeria">
+    `;   
+
+    const overlay = document.createElement ('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add ("overlay");
+    overlay.onclick = function() {
+        const body = document.querySelector("body");
+        body.classList.remove('fijar-body');
+        overlay.remove();    }
+    
+
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function() {
+        const body = document.querySelector("body");
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+    overlay.appendChild(cerrarModal)
+
+
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
+
+} 
